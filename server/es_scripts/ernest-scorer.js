f7146f2ba0936028298d9a431e8ab5a49e48f033
ben <ben@gophronesis.com>
2016-05-17 18:03:04 +0000

    modifying functions

--- server/es_scripts/ernest-scorer.js
+++ server/es_scripts/ernest-scorer.js
@@ -2,15 +2,23 @@ function combine_scores (scores, params) {
     // ** Temporary **
     // Need to actually determine how to combine these things...
     // User defined?
-    return out['symbology'] + out['delinquency'];
+    return scores['symbology'] + scores['delinquency'];
 }
 
+// Register functions
 var functions = {
     "symbology"   : symbology,
     "delinquency" : delinquency
 }
 
-var scores = {}
-for(k in params) { scores[k] = functions[k](_source[k], params[k]); }
+function run() {
+    // Compute scores (if parameterized)
+    var scores = {}
+    for(k in params) {
+        scores[k] = functions[k](_source[k], params[k]);
+    }
 
-score ? combine_scores(scores, params) : scores;
+    score ? combine_scores(scores, params) : scores;    
+}
+
+run()
