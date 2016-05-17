1885fb06d506e7601d6d1ee4a09b126fe2c66d6a
Ben Johnson <ben@gophronesis.com>
2016-05-17 13:29:51 -0400

    enforcing standard format

--- server/es_scripts/ernest-scorer.js
+++ server/es_scripts/ernest-scorer.js
@@ -1,19 +1,18 @@
-function combine_scores(scores) {
-    return scores['delinquency'];
+function combine_scores (scores) {
+  return scores['delinquency']
 }
 
 var scores = {
-    "symbology"   : -1,
-    "delinquency" : -1
+  'symbology': -1,
+  'delinquency': -1
 }
 
-if(params.symbology != null) {
-    scores['symbology'] = symbology(_source.symbology, params.symbology);
+if (params.symbology != null) {
+  scores['symbology'] = symbology(_source.symbology, params.symbology)
 }
 
-if(params.delinquency != null) {
-    scores['delinquency'] = delinquency(_source.delinquency, params.delinquency);
+if (params.delinquency != null) {
+  scores['delinquency'] = delinquency(_source.delinquency, params.delinquency)
 }
 
 combine_scores(scores)
-
--- server/es_scripts/symbology-function.js
+++ server/es_scripts/symbology-function.js
@@ -1,11 +1,10 @@
- symbology = function (data, params) {
-    var n = 0
-    data = data != null ? data : []
+symbology = function (data, params) {
+  var n = 0
+  data = data != null ? data : []
 
-    for (i = 0; i < data.length; i++) {
-      var field = data[i].field == params.field
-      n += (field & time_filter(data[i].new_date, params.min_date, params.max_date))
-    }
-    return n
+  for (i = 0; i < data.length; i++) {
+    var field = data[i].field == params.field
+    n += (field & time_filter(data[i].new_date, params.min_date, params.max_date))
   }
-
+  return n
+}
