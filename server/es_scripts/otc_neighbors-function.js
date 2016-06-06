e59f47ee5ec132b8b4cdf5937eb3c0369f20794f
ben <ben@gophronesis.com>
2016-06-06 22:45:05 +0000

    otc_neighbors-function.js

--- server/es_scripts/otc_neighbors-function.js
+++ server/es_scripts/otc_neighbors-function.js
@@ -4,14 +4,14 @@ function otc_neighbors(data, params) {
   var cond2 = false;
   
   if(data != null) {
-    prop = 100 * data.otc_count / data.total_count;
+    prop = Math.round(100 * data.otc_paths / data.total_paths);
     cond1 = data.total_count >= params.number_of_neighbors[0]
     cond2 = data.total_count <= params.number_of_neighbors[1]
   }
   
   return {
     "value" : prop,
-    "is_flag" : cond1 & cond1 & (prop > params.threshold),
+    "is_flag" : (cond1 & cond1 & (prop > params.threshold)) > 0,
     "have" : true
   }
 }
