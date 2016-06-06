3dedd8ed52116a2d133224615457b7aeb3b5de63
ben <ben@gophronesis.com>
2016-06-06 23:36:45 +0000

    es scoring functions

--- server/es_scripts/otc_neighbors-function.js
+++ server/es_scripts/otc_neighbors-function.js
@@ -6,14 +6,14 @@ function otc_neighbors(data, params) {
 
   if(data != null) {
     prop = Math.round(100 * data.otc_paths / data.total_paths);
-    // cond1 = data.total_count >= params.number_of_neighbors[0]
-    // cond2 = data.total_count <= params.number_of_neighbors[1]
+    cond1 = data.total_count >= params.number_of_neighbors[0]
+    cond2 = data.total_count <= params.number_of_neighbors[1]
   }
   
   return {
     "value" : prop,
-    // "is_flag" : (cond1 & cond1 & (prop > params.threshold)) > 0,
-    "is_flag" : prop > 0,
+    "is_flag" : (cond1 & cond1 & (prop > params.threshold)) > 0,
+    // "is_flag" : prop > 0,
     "have" : have
   }
 }
