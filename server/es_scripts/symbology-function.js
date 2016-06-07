9aa0fa616107323bfb09ed5a50b016a3f7810f8c
ben <ben@gophronesis.com>
2016-06-07 17:01:57 +0000

    updating scoring functions

--- server/es_scripts/symbology-function.js
+++ server/es_scripts/symbology-function.js
@@ -1,10 +1,11 @@
 function symbology (data, params) {
   var n = 0;
   var have = data != null;
-  data = data != null ? data : [];
-  for (i = 0; i < data.length; i++) {
-    var field = data[i].field == params.field;
-    n += (field & time_filter(data[i].new_date, params.min_date, params.max_date));
+  if(have) {
+      for (i = 0; i < data.length; i++) {
+        var field = data[i].field == params.field;
+        n += (field & time_filter(data[i].new_date, params.min_date, params.max_date));
+      }      
   }
   return {
     "value" : n,
