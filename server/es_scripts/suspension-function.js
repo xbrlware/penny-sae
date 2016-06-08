6ed026ca80fcac8563c874e482c270fb8a6f97b9
ben <ben@gophronesis.com>
2016-06-08 18:58:41 +0000

    turning dummy suspension off

--- server/es_scripts/suspension-function.js
+++ server/es_scripts/suspension-function.js
@@ -1,14 +1,14 @@
 function suspensions (data, params) {
-  // var n = 0;
-  // var have = data != null;
-  // if (have) {
-  //   for (i = 0; i < data.length; i++) {
-  //     n += time_filter(data[i].date, params.min_date, params.max_date);
-  //   }
-  // }
+  var n = 0;
+  var have = data != null;
+  if (have) {
+    for (i = 0; i < data.length; i++) {
+      n += time_filter(data[i].date, params.min_date, params.max_date);
+    }
+  }
   return {
-    'value': 100,
-    'is_flag': true,
-    'have': true
+    'value': n,
+    'is_flag': n > 0,
+    'have': have
   };
 }
