6d7a465702429401a0113663953694cb63766290
Morgan Peterson <morgan@gophronesis.com>
2016-06-29 12:41:07 -0400

    Fixed an undefined error in date splits

--- web/js/app/associates.js
+++ web/js/app/associates.js
@@ -1,5 +1,5 @@
 // web/js/app/associates.js
-/* global Ember, App, _, gconfig, alert */
+/* global Ember, App, _, gconfig */
 
 App.AssociatesRoute = Ember.Route.extend({
   model: function () {
@@ -18,33 +18,32 @@ App.AssociatesController = Ember.ObjectController.extend({
   // >>
   // For table
 
-  rgraph_edges: [],
+  rGraphEdges: [],
   update_data: function (rgraph) {
-    var rgraph_edges = _.chain(rgraph.graph.edges).map(function (v1, source) {
+    var rGraphEdges = _.chain(rgraph.graph.edges).map(function (v1, source) {
       return _.chain(v1).map(function (edge, target) {
         return {'source': source, 'target': target, 'data': edge.data};
-      }).filter(function (x) {return x.source == x.data.ownerCik;}).value();
+      }).filter(function (x) { return x.source === x.data.ownerCik; }).value();
     }).flatten().value();
-    this.set('rgraph_edges', rgraph_edges);
+    this.set('rGraphEdges', rGraphEdges);
   },
 
   tableDiv: '#associates-table',
   tableContent: function () {
-    var this_ = this;
-    return _.map(this.get('rgraph_edges'), function (x) {
+    return _.map(this.get('rGraphEdges'), function (x) {
       return [
         x.data.issuerName,
         x.data.ownerName,
         x.data.issuerCik,
         x.data.ownerCik,
-        x.data.min_date.split('-').join('/'),
-        x.data.max_date.split('-').join('/'),
+        x.data.min_date ? x.data.min_date.split('-').join('/') : null,
+        x.data.max_date ? x.data.max_date.split('-').join('/') : null,
         x.data.isDirecto === 1 ? 'X' : '',
         x.data.isOfficer === 1 ? 'X' : '',
         x.data.isTenPercentOwner === 1 ? 'X' : ''
       ];
     });
-  }.property('rgraph_edges'),
+  }.property('rGraphEdges'),
 
   tableColumns: [
     {title: 'Issuer Name'},
