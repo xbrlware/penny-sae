f126a380f7ab776b26d0388413c4e37dfd6d1d15
ben <ben@gophronesis.com>
2016-07-05 15:48:59 +0000

    adding paren

--- server/es_scripts/delinquency-function.js
+++ server/es_scripts/delinquency-function.js
@@ -5,7 +5,7 @@ function delinquency (data, params) {
     for (i = 0; i < data.length; i++) {
       var is_late = data[i].is_late == true;
       var form = data[i].form == params.form;
-      n += is_late & form & time_filter(data[i].deadline, params.min_date, params.max_date);
+      n += (is_late & form & time_filter(data[i].deadline, params.min_date, params.max_date));
     }
   }
   return {
