e620c3b1053e0b14b3f381711a2a451f16f9b0c4
bkj <ben@gophronesis.com>
2016-08-17 12:30:04 -0400

    scaffolding for results view

--- web/js/app/client.js
+++ web/js/app/client.js
@@ -68,6 +68,48 @@ App.SearchResultsView = Ember.View.extend({
   }
 });
 
+App.SummaryView = Ember.View.extend({
+  columns: [
+    {title: 'Min Date', defaultContent: '', className: 'dt-body-right'},
+    {title: 'Name', defaultContent: ''},
+    {title: 'Ticker', defaultContent: ''},
+    {title: 'SIC', defaultContent: ''}
+  ],
+
+  didInsertElement: function () {
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, this.afterRenderEvent);
+  },
+
+  afterRenderEvent: function () {
+    var _this = this;
+    console.log('hits !!', this.get('hits'));
+    var cik = this.get('hits')[0]['cik'];
+    var columns = this.get('columns');
+
+    App.Search.fetch_data('company_table', {'cik': cik}).then(function (response) {
+      Ember.$('#search_result_test').DataTable({
+        fnDrawCallback: function (oSettings) {
+          if (oSettings._iDisplayLength > oSettings._iRecordsDisplay) {
+            Ember.$(oSettings.nTableWrapper).find('.dataTables_paginate').hide();
+          }
+        },
+        columns: columns,
+        data: _.map(response['table'], function (x) {
+          return [_this.dateConversion(x.min_date), x.name, x.ticker, x.sic];
+        }),
+        bFilter: false,
+        bInfo: false
+      });
+    });
+  },
+
+  dateConversion: function (d) {
+    var date = new Date(d);
+    return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+  }
+})
+
 App.Search = Ember.Object.extend({});
 
 App.Search.reopenClass({
