7ae2680b6da39e2eeb87f45db66c9484d4709035
ben <ben@gophronesis.com>
2016-08-17 14:42:19 +0000

    updating script name

--- server/es_scripts/minify.sh
+++ server/es_scripts/minify.sh
@@ -3,16 +3,15 @@
 COMPRESS=./compressJS.sh/compressjs.sh
 
 $COMPRESS helpers.js *function.js ernest-scorer.js ernest.js
-# sudo cp ernest.js /srv/software/elasticsearch-2.2.1/config/scripts/ernest.js
 
 SCRIPT=$(cat ernest.js | sed 's/"/\\"/g')
-curl -XPOST http://localhost:9205/_scripts/js/ernest_test -d  "{
+curl -XPOST http://localhost:9205/_scripts/js/ernest_v2 -d  "{
      \"script\": \"$SCRIPT\"
 }"
 
 echo
 
-curl -XPOST http://localhost:9205/_scripts/javascript/ernest_test -d  "{
+curl -XPOST http://localhost:9205/_scripts/javascript/ernest_v2 -d  "{
      \"script\": \"$SCRIPT\"
 }"
 
