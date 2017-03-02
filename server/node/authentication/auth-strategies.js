1f11ef7168a0d0ef7c329185169c5440230682ce
Morgan Peterson <morgan@gophronesis.com>
2017-03-02 18:47:03 -0500

    Login page and frontpage installed

--- server/node/authentication/auth-strategies.js
+++
@@ -1,9 +0,0 @@
-// server/node/authentication/auth-strategies.js
-
-module.exports = function (passport, makeToken, config) {
-  return {
-    local: require('./local-auth-functions')(passport, config, makeToken),
-    ldap: require('./ldap-auth-functions')(passport, config, makeToken),
-    gated: require('./gated-auth-functions')(passport, config, makeToken)
-  };
-};
